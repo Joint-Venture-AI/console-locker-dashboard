9ef047e6de808746c2b503f56627ba7726c3446a
Raiyan109 <kraiyan109@gmail.com>
2024-12-24 12:14:42 +0600

    driver request cards done

--- src/constants/driver.constants.jsx
+++ src/constants/driver.constants.jsx
@@ -21,5 +21,89 @@ export const driverItems = [
             name: "Driver picture.pdf",
             image: pdfImg
         },
-    }
+    },
+    {
+        name: "Dianne Russell",
+        image: driverImage1,
+        designation: "Driver",
+        doc1:{
+            name: "NID.pdf",
+            image: pdfImg
+        },
+        doc2:{
+            name: "Driver License.pdf",
+            image: pdfImg
+        },
+        doc3:{
+            name: "Vehicle front.pdf",
+            image: pdfImg
+        },
+        doc4:{
+            name: "Driver picture.pdf",
+            image: pdfImg
+        },
+    },
+    {
+        name: "Dianne Russell",
+        image: driverImage1,
+        designation: "Driver",
+        doc1:{
+            name: "NID.pdf",
+            image: pdfImg
+        },
+        doc2:{
+            name: "Driver License.pdf",
+            image: pdfImg
+        },
+        doc3:{
+            name: "Vehicle front.pdf",
+            image: pdfImg
+        },
+        doc4:{
+            name: "Driver picture.pdf",
+            image: pdfImg
+        },
+    },
+    {
+        name: "Dianne Russell",
+        image: driverImage1,
+        designation: "Driver",
+        doc1:{
+            name: "NID.pdf",
+            image: pdfImg
+        },
+        doc2:{
+            name: "Driver License.pdf",
+            image: pdfImg
+        },
+        doc3:{
+            name: "Vehicle front.pdf",
+            image: pdfImg
+        },
+        doc4:{
+            name: "Driver picture.pdf",
+            image: pdfImg
+        },
+    },
+    {
+        name: "Dianne Russell",
+        image: driverImage1,
+        designation: "Driver",
+        doc1:{
+            name: "NID.pdf",
+            image: pdfImg
+        },
+        doc2:{
+            name: "Driver License.pdf",
+            image: pdfImg
+        },
+        doc3:{
+            name: "Vehicle front.pdf",
+            image: pdfImg
+        },
+        doc4:{
+            name: "Driver picture.pdf",
+            image: pdfImg
+        },
+    },
 ]
