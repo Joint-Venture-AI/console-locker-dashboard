1472a8e0197e1ac9d2e83eeee38db32299754497
sharif57 <sharifmahamud577951@gmail.com>
2025-01-31 09:46:40 +0600

    done

--- src/pages/Main/DashboardHome/DashboardHome.jsx
+++ src/pages/Main/DashboardHome/DashboardHome.jsx
@@ -17,14 +17,14 @@ const DashboardHome = () => {
         <div className="flex items-center justify-center gap-6 px-[24px] border border-black  py-[20px] rounded-lg space-y-3 bg-white w-96 md:w-full">
           <div className="text-center">
             <h3 className="text-[20px]">{"Total Buy"}</h3>
-            <h3 className="text-[30px] font-extralight">1250</h3>
+            <h3 className="text-[30px] font-extralight">$1250</h3>
           </div>
         </div>
 
         <div className="flex items-center justify-center gap-6 border border-black px-[24px] py-[20px] rounded-lg space-y-3 bg-white w-96 md:w-full">
           <div className="text-center">
             <h3 className="text-[20px]">{"Total Earnings"}</h3>
-            <h3 className="text-[30px] font-extralight">740</h3>
+            <h3 className="text-[30px] font-extralight">$740</h3>
           </div>
         </div>
       </div>
