0e3099bcd7548670ac839ab84f84635106fbe087
shaishabcoding <shaishabchandrashil@gmail.com>
2025-04-15 17:39:50 +0600

    fix

--- src/pages/Main/ProductInfo/ProductInfo.jsx
+++ src/pages/Main/ProductInfo/ProductInfo.jsx
@@ -33,7 +33,7 @@ const ProductInfo = () => {
 		<div className="bg-white p-8">
 			<div className="flex justify-between items-center mb-6">
 				<div className="flex items-center gap-2">
-					<Link to={"/product-info"}>
+					<Link to={"/"}>
 						<Button
 							type="link"
 							icon={<ArrowLeft />}
