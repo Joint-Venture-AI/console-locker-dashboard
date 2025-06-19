793d7cbb077ee41ce9097077962d1b8d09b15288
sharif57 <sharifmahamud577951@gmail.com>
2025-06-19 16:21:10 +0600

    done

--- src/redux/features/buyProductSlice.js
+++ src/redux/features/buyProductSlice.js
@@ -1,46 +1,47 @@
 import baseApi from "../api/baseApi";
 
 export const transactionApi = baseApi.injectEndpoints({
-	endpoints: (builder) => ({
-		allBuyProductGet: builder.query({
-			query: ({ limit, state, page }) => {
-				let queryParams = new URLSearchParams();
-				if (limit) queryParams.append("limit", limit.toString());
-				if (state) queryParams.append("state", state.toString());
-				if (page) queryParams.append("page", page.toString());
-				return `/admin/buy?${queryParams.toString()}`;
-			},
-			providesTags: ["Buy"],
-		}),
+  endpoints: (builder) => ({
+    allBuyProductGet: builder.query({
+      query: ({ limit, state, page }) => {
+        let queryParams = new URLSearchParams();
+        if (limit) queryParams.append("limit", limit.toString());
+        if (state) queryParams.append("state", state.toString());
+        if (page) queryParams.append("page", page.toString());
+        return `/admin/buy?${queryParams.toString()}`;
+      },
+      providesTags: ["Buy"],
+    }),
 
-		productCancle: builder.mutation({
-			query: (id) => ({
-				url: `/admin/buy/${id}/cancel`,
-				method: "POST",
-			}),
-			invalidatesTags: ["Buy"],
-		}),
+    productCancle: builder.mutation({
+      query: (id) => ({
+        url: `/admin/buy/${id}/cancel`,
+        method: "POST",
+      }),
+      invalidatesTags: ["Buy"],
+    }),
 
-		confirmProduct: builder.mutation({
-			query: (id) => ({
-				url: `/admin/buy/${id}`,
-				method: "POST",
-			}),
-			invalidatesTags: ["Buy"],
-		}),
-		sendReceipt: builder.mutation({
-			query: () => ({
-				url: '/admin/buy/send-mail',
-				method: "POST",
-			}),
-			invalidatesTags: ["Buy"],
-		})
-	}),
+    confirmProduct: builder.mutation({
+      query: (id) => ({
+        url: `/admin/buy/${id}`,
+        method: "POST",
+      }),
+      invalidatesTags: ["Buy"],
+    }),
+    sendReceipt: builder.mutation({
+      query: (body) => ({
+        url: "/admin/buy/send-mail",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["Buy"],
+    }),
+  }),
 });
 
 export const {
-	useAllBuyProductGetQuery,
-	useProductCancleMutation,
-	useConfirmProductMutation,
-	useSendReceiptMutation
+  useAllBuyProductGetQuery,
+  useProductCancleMutation,
+  useConfirmProductMutation,
+  useSendReceiptMutation,
 } = transactionApi;
